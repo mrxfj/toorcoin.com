d5e69386d3306bf7182888aca2396b9a5b69ca4b
mxrfj <36507152+mrxfj@users.noreply.github.com>
2018-02-17 10:23:12 +0800

    coming soon image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Image } from 'semantic-ui-react'
+
+import comingsoon from './toorcoin.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Image src={comingsoon} fluid />
+      </Container>
     );
   }
 }
