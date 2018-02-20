15bfc4f490d951f301689a3455a7006322db7973
mxrfj <36507152+mrxfj@users.noreply.github.com>
2018-02-20 20:24:51 +0800

    Adding css which is not imported atm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import { Container, Image } from 'semantic-ui-react'
+import { Image } from 'semantic-ui-react'
 
 import comingsoon from './toorcoin.png'
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Image src={comingsoon} fluid />
-      </Container>
+      <Image src={comingsoon} fluid />
     );
   }
 }
